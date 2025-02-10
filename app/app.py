f6fa9bc8b1c50dcef418d71b36e9a17297b7cd9b
Timjini <hatim.jini@gmail.com>
2025-02-10 13:37:20 +0100

    all changes

--- app/app.py
+++ app/app.py
@@ -18,20 +18,6 @@ try:
             time.sleep(5)  
             print("table not found")
 
-        insert_fake_data(connection, "products", 1000)
-
-        # try:
-        #     insert_fake_data(connection, "products", 1000)
-        #     insert_fake_data(connection, "shoppers", 1000)
-        #     insert_fake_data(connection, "vendors", 1000)
-        #     # insert_order_items(cursor, num_order_items=5, num_orders=1000, num_products=100, num_vendors=50)
-        #     # insert_vendor_products(cursor, num_vendors=1000, num_products=1000)
-        #     # connection.commit()
-        #     print("Fake data inserted successfully")
-        # except Exception as e:
-        #     print(f"Error inserting data: {e}")
-        #     connection.rollback()
-
 except Error as e:
     print(f"Error: {e}")
 
